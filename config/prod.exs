61fd47ecc936e35b91d16f102f9d064b68a712c0
Lars Bünting <lars.buenting@smacc.io>
2017-01-09 14:40:47 +0100

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -58,4 +58,17 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+
+config :peep, Peep.Endpoint,
+  http: [port: {:system, "PORT"}],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "frozen-coast-87972.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :peep, Peep.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
+
+# Do not print debug messages in production
+config :logger, level: :info
